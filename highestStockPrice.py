2613116e0e78a7763e9ab8ba78fdb69dca208ae7
Bill Ying <bill_ying@hotmail.com>
2019-03-14 20:11:46 -0400

    Create exe

--- highestStockPrice.py
+++ highestStockPrice.py
@@ -1,10 +1,18 @@
 
-import json, requests
+import json, sys
+import requests
 
 alphaVantageKey = '&apikey=OYAJLARX2ZAA2T1B'
 url ="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol="
 
-urlWithTicker = url+'MSFT'+alphaVantageKey
-myResponse = requests.get(urlWithTicker)
-
-print(myResponse.content)
+if len(sys.argv) == 1:
+    print("Usage: highestStockPrice ticker YYYY-MM-DD YYYY-MM-DD")
+else:
+    urlWithTicker = url+sys.argv[1]+alphaVantageKey
+    myResponse = requests.get(urlWithTicker)
+    jsonData = myResponse.json()
+    key = list(jsonData)[1]
+    prices = jsonData[key]
+    pricesInDates = {k: v for k, v in prices.items() if k >= sys.argv[2] and k <= sys.argv[3]}
+    dateKey = max(pricesInDates, key=lambda x: float(pricesInDates[x]['4. close']))
+    print(dateKey, pricesInDates[dateKey]['4. close'])
