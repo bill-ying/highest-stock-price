22feeb98e279f081fb37423d3cb88ed53998290b
Bill Ying <bill_ying@hotmail.com>
2021-02-26 22:53:32 -0500

    Update readme and cleanup folder

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# highestStockPrice
+# highest-stock-price
+
+This program retrieves the highest closing price of a security within a period.
+
+The program depends on package "requests".
+
+To run the program:
+- python highest_stock_price ticker start_date(YYYY-MM-DD) [optional_end_date(YYYY-MM-DD)]
+
+The security can be listed in New York, London, or Toronto:
+- New York listed security, use ticker directly (e.g. AAPL)
+- London listed security, use ticker.LON (e.g. BARC.LON)
+- Toronto Listed security, use ticker.TO (e.g. SU.TO)
--- highest_stock_price.py
+++ highest_stock_price.py
@@ -1,18 +1,30 @@
+import argparse
+import datetime
 
-import json, sys
 import requests
 
-alphaVantageKey = '&apikey=OYAJLARX2ZAA2T1B'
-url ="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol="
+# This is my Alpha Vantage key.  Please register your own Alpha Vantage key and replace with it in the following line
+ALPHA_VANTAGE_KEY = 'OYAJLARX2ZAA2T1B'
+STOCK_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&apikey=' \
+            + ALPHA_VANTAGE_KEY + '&symbol='
+CLOSE_PRICE_KEY = '4. close'
 
-if len(sys.argv) == 1:
-    print("Usage: highestStockPrice ticker YYYY-MM-DD(start) YYYY-MM-DD(end)")
-else:
-    urlWithTicker = url+sys.argv[1]+alphaVantageKey
-    myResponse = requests.get(urlWithTicker)
-    jsonData = myResponse.json()
-    key = list(jsonData)[1]
-    prices = jsonData[key]
-    pricesInDates = {k: v for k, v in prices.items() if k >= sys.argv[2] and k <= sys.argv[3]}
-    dateKey = max(pricesInDates, key=lambda x: float(pricesInDates[x]['4. close']))
-    print(dateKey, "${0:.2f}".format(float(pricesInDates[dateKey]['4. close'])))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Retrieve highest closing price of a security between certain dates')
+    parser.add_argument('ticker', help='security ticker')
+    parser.add_argument('start_date', help='start date in YYYY-MM-DD inclusive')
+    parser.add_argument('end_date', nargs='?', default=datetime.datetime.today().strftime('%Y-%m-%d'),
+                        help='optional end date in YYYY-MM-DD inclusive (default is current date)')
+    args = parser.parse_args()
+    stock_response = requests.get(STOCK_URL + args.ticker).json()
+
+    if 'Error Message' in stock_response:
+        print('Ticker ' + args.ticker + ' is invalid')
+        exit(1001)
+    else:
+        prices = stock_response['Time Series (Daily)']
+        prices_in_dates = {k: v for k, v in prices.items() if args.start_date <= k <= args.end_date}
+        date_for_highest_price = max(prices_in_dates, key=lambda x: float(prices_in_dates[x][CLOSE_PRICE_KEY]))
+        currency_symbol = '\u00a3' if str(args.ticker).endswith('.LON') else '$'
+        print(date_for_highest_price,
+              currency_symbol + '{0:.2f}'.format(float(prices_in_dates[date_for_highest_price][CLOSE_PRICE_KEY])))
