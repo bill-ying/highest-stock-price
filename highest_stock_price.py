a583b98a3a555077853df16de323470e8ca1980d
Bill Ying <bill_ying@hotmail.com>
2021-02-26 22:51:08 -0500

    Update readme and add GBP symbol

---
+++ highest_stock_price.py
@@ -0,0 +1,18 @@
+
+import json, sys
+import requests
+
+alphaVantageKey = '&apikey=OYAJLARX2ZAA2T1B'
+url ="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol="
+
+if len(sys.argv) == 1:
+    print("Usage: highestStockPrice ticker YYYY-MM-DD(start) YYYY-MM-DD(end)")
+else:
+    urlWithTicker = url+sys.argv[1]+alphaVantageKey
+    myResponse = requests.get(urlWithTicker)
+    jsonData = myResponse.json()
+    key = list(jsonData)[1]
+    prices = jsonData[key]
+    pricesInDates = {k: v for k, v in prices.items() if k >= sys.argv[2] and k <= sys.argv[3]}
+    dateKey = max(pricesInDates, key=lambda x: float(pricesInDates[x]['4. close']))
+    print(dateKey, "${0:.2f}".format(float(pricesInDates[dateKey]['4. close'])))
